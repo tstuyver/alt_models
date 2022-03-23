033bb71980bf3b7aee46a0ab2367c71796c5b045
tstuyver <thijsstuyver@hotmail.com>
2022-03-23 15:46:01 -0400

    Add argument parser to get_desc_for_corr_cycloadd.py

--- get_desc_for_corr_cycloadd.py
+++ get_desc_for_corr_cycloadd.py
@@ -5,11 +5,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import rdkit.Chem as Chem
 import mol_graph_cycloadd
+from argparse import ArgumentParser
 from scaling import min_max_normalize, min_max_normalize_reaction,reaction_to_reactants,scale_targets
 
 
-elem_list = ['C', 'O', 'N', 'F', 'Br', 'Cl', 'S',
-             'Si', 'B', 'I', 'K', 'Na', 'P', 'Mg', 'Li', 'Al', 'H']
+parser = ArgumentParser()
+parser.add_argument('--csv_file', type=str, required=True,
+                    help='path to file containing the rxn-smiles and targets')
+parser.add_argument('--atom_descs_file', type=str, required=True,
+                    help='path to .pkl-file containing atom descriptors')
+parser.add_argument('--reaction_descs_file', type=str, required=True,
+                    help='path to .pkl-file containing reaction descriptors')                    
 
 
 def convert_str_to_array(string):
@@ -47,63 +53,53 @@ def get_aromatic_list(smiles):
 
     return arom_list
 
-targets = pd.read_csv('output_attempt2_mapped.csv')[['smiles','DG_TS']]
-descriptors = pd.read_pickle('atom_desc_cycloadd_wln.pkl')
-reactants = reaction_to_reactants(descriptors['smiles'].tolist())
-descriptors,_ = min_max_normalize(descriptors.copy(), train_smiles=reactants)
-descriptors.to_csv('atom_desc_scaled.csv')
-mol_descs = pd.read_csv('reaction_desc_cycloadd_wln.csv')
-#mol_descs,_ = min_max_normalize_reaction(mol_descs.copy(), train_smiles=targets['smiles'])
-#mol_descs.to_csv('reaction_desc_scaled.csv')
 
-reactions = pd.merge(mol_descs, targets, on='smiles')
-reactions['reactants'] = reactions['smiles'].apply(lambda x: x.split('>')[0])
-reactions['products'] = reactions['smiles'].apply(lambda x: x.split('>')[-1])
+if __name__ == '__main__':
+    args = parser.parse_args()
+    targets = pd.read_csv(args.csv_file)[['smiles','DG_TS']]
+    descriptors = pd.read_pickle(args.atom_descs_file)
+    reactants = reaction_to_reactants(descriptors['smiles'].tolist())
+    descriptors,_ = min_max_normalize(descriptors.copy(), train_smiles=reactants)
+    descriptors.to_csv('atom_desc_scaled.csv')
+    mol_descs = pd.read_pickle(args.reaction_descs_file)
+    #mol_descs,_ = min_max_normalize_reaction(mol_descs.copy(), train_smiles=targets['smiles'])
+    #mol_descs.to_csv('reaction_desc_scaled.csv')
 
-#reactions = reactions[['reactants', 'products']]
-#descriptors = descriptors.set_index('smiles')
-reactions['desc_core'] = reactions.apply(lambda x: mol_graph_cycloadd.smiles2graph_pr(descriptors, x['reactants'], x['products']), axis=1)
+    reactions = pd.merge(mol_descs, targets, on='smiles')
+    reactions['reactants'] = reactions['smiles'].apply(lambda x: x.split('>')[0])
+    reactions['products'] = reactions['smiles'].apply(lambda x: x.split('>')[-1])
 
-reactions = reactions[reactions['desc_core'] != 'lol']
+    #reactions = reactions[['reactants', 'products']]
+    #descriptors = descriptors.set_index('smiles')
+    reactions['desc_core'] = reactions.apply(lambda x: mol_graph_cycloadd.smiles2graph_pr(descriptors, x['reactants'], x['products']), axis=1)
 
-reaction_feature_list = []
+    reactions = reactions[reactions['desc_core'] != 'lol']
 
-tmp_list = reactions['desc_core'].values.tolist()
-tmp_list_Er = reactions['E_r'].values.tolist()
-tmp_list_G = reactions['G'].values.tolist()
-tmp_list_G_alt1 = reactions['G_alt1'].values.tolist()
-tmp_list_G_alt2 = reactions['G_alt2'].values.tolist()
-tmp_list_target = reactions['DG_TS'].values.tolist() 
+    reaction_feature_list = []
 
-for idx, reaction in enumerate(tmp_list):
-    rxn_desc_array = np.array([tmp_list_Er[idx], tmp_list_G[idx], tmp_list_G_alt1[idx], tmp_list_G_alt2[idx], tmp_list_target[idx]])
-    reaction_feature_list.append(np.concatenate((np.concatenate([arr[:-1] for arr in reaction[0]]), rxn_desc_array)))
+    tmp_list = reactions['desc_core'].values.tolist()
+    tmp_list_Er = reactions['E_r'].values.tolist()
+    tmp_list_G = reactions['G'].values.tolist()
+    tmp_list_G_alt1 = reactions['G_alt1'].values.tolist()
+    tmp_list_G_alt2 = reactions['G_alt2'].values.tolist()
+    tmp_list_target = reactions['DG_TS'].values.tolist() 
+
+    for idx, reaction in enumerate(tmp_list):
+        rxn_desc_array = np.array([tmp_list_Er[idx], tmp_list_G[idx], tmp_list_G_alt1[idx], tmp_list_G_alt2[idx], tmp_list_target[idx]])
+        reaction_feature_list.append(np.concatenate((np.concatenate([arr[:-1] for arr in reaction[0]]), rxn_desc_array)))
     
-columns_list = []
+    columns_list = []
 
-for i in range(1,6):
-    for desc_name in ['partial_charge', 'fukui_elec', 'fukui_neu', 'nmr', 'spin_dens_triplet', 'sasa', 'pint']:
-        columns_list.append(f'{desc_name}_{i}') 
+    for i in range(1,6):
+        for desc_name in ['partial_charge', 'fukui_elec', 'fukui_neu', 'nmr', 'spin_dens_triplet', 'sasa', 'pint']:
+            columns_list.append(f'{desc_name}_{i}') 
 
-columns_list += ['E_r', 'G', 'G_alt1', 'G_alt2', 'DG_TS']
+    columns_list += ['E_r', 'G', 'G_alt1', 'G_alt2', 'DG_TS']
 
-print(len(reaction_feature_list[1]), len(columns_list))
+    print(len(reaction_feature_list[1]), len(columns_list))
 
-df = pd.DataFrame(reaction_feature_list, columns=columns_list)
+    df = pd.DataFrame(reaction_feature_list, columns=columns_list)
 
-print(df.head())
+    print(df.head())
 
-df.to_pickle('input_random_forest.pkl')
-
-#for i in range(len(tmp_list)):
-#    for j in range(len(tmp_list[i][0])):
-#        data_point_list.append(tmp_list[i][0][j])
-
-#df = pd.DataFrame(data_point_list, columns=['partial_charge', 'fukui_elec', 'fukui_neu', 'nmr', 'spin_dens', 'spin_dens_triplet',
-#                                            'sasa','pint','atom_type'])
-#df['atom_type'] = df['atom_type'].apply(lambda x: elem_list[int(x)])
-
-#df.to_csv('atom_desc_reactive_sites_cycloadd.csv')
-#df.to_pickle('atom_desc_reactive_sites_cycloadd.pkl')
-
-
+    df.to_pickle('input_alt_models.pkl')
